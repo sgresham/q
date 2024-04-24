c29dc9d951882fa9880d97d73e45c860b6c7732e
Steve Gresham <steve@gresham.id.au>
2024-04-24 14:09:26 +1000

    ?

--- transcription/server/python_server.py
+++ transcription/server/python_server.py
@@ -188,13 +188,6 @@ async def transcribe_and_send(client_id, websocket, new_audio_data):
 
     # Check the return code of the process
     if result.returncode == 0:
-            # Parse the JSON data
-            print('{dirty DEBUG}: ',result.stdout)
-            parsed_data = json.loads(result.stdout)
-            print('{parsed DEBUG}: ',parsed_data)
-            # Get the "sections" part
-            sections = parsed_data.get("track", {}).get("sections", [])
-            print('{SECTION DEBUG}: ',sections)
             music.info(result.stdout)
     else:
         print("Command failed with return code:", result.returncode)
