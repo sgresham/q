56142e39f20a03b11d00fd02cff34ae69d3cbc22
Steve Gresham <steve@gresham.id.au>
2024-05-10 10:25:00 +1000

    trying new whisper model

--- transcription/server/python_server.py
+++ transcription/server/python_server.py
@@ -17,7 +17,7 @@ import torch
 import pathlib
 import subprocess
 
-from transformers import pipeline
+from transformers import pipeline, AutoModelForSpeechSeq2Seq, AutoProcessor
 from pyannote.core import Segment
 from pyannote.audio import Model
 from pyannote.audio.pipelines import VoiceActivityDetection
@@ -25,20 +25,48 @@ from pyannote.audio.pipelines import VoiceActivityDetection
 import sys
 import os
 
+# Check if CUDA is available
+if torch.cuda.is_available():
+    print("CUDA is available!")
+    # Print the number of CUDA devices
+    print("Number of CUDA devices:", torch.cuda.device_count())
+    device = 'cuda'
+else:
+    print("CUDA is not available.")
+    device = 'cpu'
+
+# i am running P40s that dont support float16, you should change int8 to float16 if your card/s do support it
+torch_dtype = torch.int8 if torch.cuda.is_available() else torch.float32
+
+# Set up our Models
+cheap_model_id = "distil-whisper/distil-large-v3"
+expensive_model_id = "openai/whisper-large-v3"
+model_id = cheap_model_id
+# get your key here -> https://huggingface.co/pyannote/segmentation
+VAD_AUTH_TOKEN = "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"
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
 # Add the root directory of the project to the Python path
-root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))  
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 sys.path.append(root_dir)
 
 # Create a logger
 logger = logging.getLogger('q_root')
 transcript = logging.getLogger('transcript')
 music = logging.getLogger('music')
-logging.getLogger("websockets").setLevel(logging.WARNING)  # Adjust the logger name as needed
+logging.getLogger("websockets").setLevel(
+    logging.WARNING)  # Adjust the logger name as needed
 
 
 # Define the music recognition tool command and its arguments
 command = "songrec"
 arguments = ["audio-file-to-recognized-song"]
+
 
 def setup_logging():
     config_file = pathlib.Path("log_config/config.json")
@@ -47,42 +75,38 @@ def setup_logging():
     logging.config.dictConfig(config)
 
 
-# Check if CUDA is available
-if torch.cuda.is_available():
-    print("CUDA is available!")
-    # Print the number of CUDA devices
-    print("Number of CUDA devices:", torch.cuda.device_count())
-    device='cuda'
-else:
-    print("CUDA is not available.")
-    device='cpu'
+# Define Audio Constants
 
 HOST = '0.0.0.0'
 PORT = 8765
 SAMPLING_RATE = 16000
 AUDIO_CHANNELS = 1
-SAMPLES_WIDTH = 2 # int16
+SAMPLES_WIDTH = 2  # int16
 DEBUG = False
-VAD_AUTH_TOKEN = "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" # get your key here -> https://huggingface.co/pyannote/segmentation
 
 DEFAULT_CLIENT_CONFIG = {
-    "language" : None, # multilingual
-    "chunk_length_seconds" : 5,
-    "chunk_offset_seconds" : 0.5
+    "language": None,  # multilingual
+    "chunk_length_seconds": 5,
+    "chunk_offset_seconds": 0.5
 }
-
 
 
 audio_dir = "audio_files"
 os.makedirs(audio_dir, exist_ok=True)
 
-## ---------- INSTANTIATES VAD --------
-model = Model.from_pretrained("pyannote/segmentation", use_auth_token=VAD_AUTH_TOKEN)
+# ---------- INSTANTIATES VAD --------
+model = Model.from_pretrained(
+    "pyannote/segmentation", use_auth_token=VAD_AUTH_TOKEN)
 vad_pipeline = VoiceActivityDetection(segmentation=model)
-vad_pipeline.instantiate({"onset": 0.5, "offset": 0.5, "min_duration_on": 0.3, "min_duration_off": 0.3})
+vad_pipeline.instantiate(
+    {"onset": 0.5, "offset": 0.5, "min_duration_on": 0.3, "min_duration_off": 0.3})
 
-## ---------- INSTANTIATES SPEECH --------
-recognition_pipeline = pipeline("automatic-speech-recognition", model="openai/whisper-large-v3", device=device)
+# ---------- INSTANTIATES SPEECH --------
+recognition_pipeline = pipeline("automatic-speech-recognition", model=model, tokenizer=processor.tokenizer,
+                                feature_extractor=processor.feature_extractor,
+                                max_new_tokens=128,
+                                torch_dtype=torch_dtype, 
+                                device=device)
 
 
 connected_clients = {}
@@ -93,28 +117,31 @@ client_configs = {}
 file_counters = {}
 
 
-
 async def transcribe_and_send(client_id, websocket, new_audio_data):
     global file_counters
 
-    if DEBUG: print(f"Client ID {client_id}: new_audio_data length in seconds at transcribe_and_send: {float(len(new_audio_data)) / float(SAMPLING_RATE * SAMPLES_WIDTH)}")
+    if DEBUG:
+        print(f"Client ID {client_id}: new_audio_data length in seconds at transcribe_and_send: {float(len(new_audio_data)) / float(SAMPLING_RATE * SAMPLES_WIDTH)}")
 
     # Initialize temporary buffer for new clients
     if client_id not in client_temp_buffers:
         client_temp_buffers[client_id] = bytearray()
 
-    if DEBUG: print(f"Client ID {client_id}: client_temp_buffers[client_id] length in seconds at transcribe_and_send: {float(len(client_temp_buffers[client_id])) / float(SAMPLING_RATE * SAMPLES_WIDTH)}")
+    if DEBUG:
+        print(
+            f"Client ID {client_id}: client_temp_buffers[client_id] length in seconds at transcribe_and_send: {float(len(client_temp_buffers[client_id])) / float(SAMPLING_RATE * SAMPLES_WIDTH)}")
 
     # Add new audio data to the temporary buffer
     old_audio_data = bytes(client_temp_buffers[client_id])
 
-    if DEBUG: print(f"Client ID {client_id}: old_audio_data length in seconds at transcribe_and_send: {float(len(old_audio_data)) / float(SAMPLING_RATE * SAMPLES_WIDTH)}")
-
+    if DEBUG:
+        print(f"Client ID {client_id}: old_audio_data length in seconds at transcribe_and_send: {float(len(old_audio_data)) / float(SAMPLING_RATE * SAMPLES_WIDTH)}")
 
     audio_data = old_audio_data + new_audio_data
 
-    if DEBUG: print(f"Client ID {client_id}: audio_data length in seconds at transcribe_and_send: {float(len(audio_data)) / float(SAMPLING_RATE * SAMPLES_WIDTH)}")
-    
+    if DEBUG:
+        print(f"Client ID {client_id}: audio_data length in seconds at transcribe_and_send: {float(len(audio_data)) / float(SAMPLING_RATE * SAMPLES_WIDTH)}")
+
     # Initialize file counter for new clients
     if client_id not in file_counters:
         file_counters[client_id] = 0
@@ -122,7 +149,8 @@ async def transcribe_and_send(client_id, websocket, new_audio_data):
     # File path
     file_name = f"{audio_dir}/{client_id}_{file_counters[client_id]}.wav"
 
-    if DEBUG: print(f"Client ID {client_id}: Filename : {file_name}")
+    if DEBUG:
+        print(f"Client ID {client_id}: Filename : {file_name}")
 
     file_counters[client_id] += 1
 
@@ -139,69 +167,80 @@ async def transcribe_and_send(client_id, websocket, new_audio_data):
     vad_time = time.time() - start_time_vad
 
     # Logging after VAD
-    if DEBUG: print(f"Client ID {client_id}: VAD result segments count: {len(result)}")
-    if DEBUG: print(f"Client ID {client_id}: VAD inference time: {vad_time:.2f}")
+    if DEBUG:
+        print(
+            f"Client ID {client_id}: VAD result segments count: {len(result)}")
+    if DEBUG:
+        print(f"Client ID {client_id}: VAD inference time: {vad_time:.2f}")
 
-    if len(result) == 0: # this should happen just if there's no old audio data
+    if len(result) == 0:  # this should happen just if there's no old audio data
         os.remove(file_name)
-        client_temp_buffers[client_id].clear() 
+        client_temp_buffers[client_id].clear()
         return
-    
-    
-    
+
     # Get last recognized segment
     last_segment = None
     for segment in result.itersegments():
         last_segment = segment
 
-    if DEBUG: print(f"Client ID {client_id}: VAD last Segment end : {last_segment.end}")
-    
+    if DEBUG:
+        print(
+            f"Client ID {client_id}: VAD last Segment end : {last_segment.end}")
+
     # if the voice ends before chunk_offset_seconds process it all
     if last_segment.end < (len(audio_data) / (SAMPLES_WIDTH * SAMPLING_RATE)) - int(client_configs[client_id]['chunk_offset_seconds']):
         start_time_transcription = time.time()
-        
+
         if client_configs[client_id]['language'] is not None:
-            result = recognition_pipeline(file_name, generate_kwargs={"language": client_configs[client_id]['language']})
+            result = recognition_pipeline(file_name, generate_kwargs={
+                                          "language": client_configs[client_id]['language']})
         else:
             result = recognition_pipeline(file_name)
         transcription_time = time.time() - start_time_transcription
-        if DEBUG: print(f"Transcription Time: {transcription_time:.2f} seconds")
+        if DEBUG:
+            print(f"Transcription Time: {transcription_time:.2f} seconds")
 
         print(f"Client ID {client_id}: Transcribed : {result['text']}")
         transcript.info(result['text'])
 
         if result['text']:
             await websocket.send(result['text'])
-            client_temp_buffers[client_id].clear() # Clear temp buffer after processing
+            # Clear temp buffer after processing
+            client_temp_buffers[client_id].clear()
     else:
         client_temp_buffers[client_id].clear()
         client_temp_buffers[client_id].extend(audio_data)
-        if DEBUG: print(f"Skipping because {last_segment.end} falls after {(len(audio_data) / (SAMPLES_WIDTH * SAMPLING_RATE)) - int(client_configs[client_id]['chunk_offset_seconds'])}")
+        if DEBUG:
+            print(
+                f"Skipping because {last_segment.end} falls after {(len(audio_data) / (SAMPLES_WIDTH * SAMPLING_RATE)) - int(client_configs[client_id]['chunk_offset_seconds'])}")
 
     # lets check to see if there is any music playing that we want to recognize.
 
     # Run the command using the subprocess module
     loopArgs = arguments + [file_name]
-    if DEBUG: print("DEBUG: Running command: ", loopArgs)
-    result = subprocess.run([command] + loopArgs, bufsize=0, capture_output=True, text=True)
-   
+    if DEBUG:
+        print("DEBUG: Running command: ", loopArgs)
+    result = subprocess.run([command] + loopArgs,
+                            bufsize=0, capture_output=True, text=True)
 
     # Check the return code of the process
     if result.returncode == 0:
-            music.info(result.stdout)
+        music.info(result.stdout)
     else:
         print("Command failed with return code:", result.returncode)
 
     # in the end always delete the created file
-    os.remove(file_name) 
+    os.remove(file_name)
+
 
 async def receive_audio(websocket, path):
     client_id = str(uuid.uuid4())
     connected_clients[client_id] = websocket
     client_buffers[client_id] = bytearray()
     client_configs[client_id] = DEFAULT_CLIENT_CONFIG
-    
-    if DEBUG: print(f"Client {client_id} connected")
+
+    if DEBUG:
+        print(f"Client {client_id} connected")
 
     try:
         async for message in websocket:
@@ -211,22 +250,29 @@ async def receive_audio(websocket, path):
                 config = json.loads(message)
                 if config.get('type') == 'config':
                     client_configs[client_id] = config['data']
-                    if DEBUG: print(f"Config for {client_id}: {client_configs[client_id]}")
+                    if DEBUG:
+                        print(
+                            f"Config for {client_id}: {client_configs[client_id]}")
                     continue
             else:
-                if DEBUG: print(f"Unexpected message type from {client_id}")
+                if DEBUG:
+                    print(f"Unexpected message type from {client_id}")
 
             # Process audio when enough data is received
             if len(client_buffers[client_id]) > int(client_configs[client_id]['chunk_length_seconds']) * SAMPLING_RATE * SAMPLES_WIDTH:
-                if DEBUG: print(f"Client ID {client_id}: receive_audio calling transcribe_and_send with length: {len(client_buffers[client_id])}")
+                if DEBUG:
+                    print(
+                        f"Client ID {client_id}: receive_audio calling transcribe_and_send with length: {len(client_buffers[client_id])}")
                 await transcribe_and_send(client_id, websocket, client_buffers[client_id])
                 client_buffers[client_id].clear()
 
     except websockets.ConnectionClosed as e:
-        if DEBUG: print(f"Connection with {client_id} closed: {e}")
+        if DEBUG:
+            print(f"Connection with {client_id} closed: {e}")
     finally:
         del connected_clients[client_id]
         del client_buffers[client_id]
+
 
 async def main():
     async with websockets.serve(receive_audio, HOST, PORT):
